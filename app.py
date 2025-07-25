d3e08f6d4b795462d3215643646ec4d168a6f35d
LikhithaLucky2004 <224g1a3247@srit.ac.in>
2025-07-25 09:03:05 +0530

    thanks

--- app.py
+++ app.py
@@ -127,6 +127,8 @@ def reports():
 
 # ---------------------- MAIN ----------------------
 
+import os
+
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
